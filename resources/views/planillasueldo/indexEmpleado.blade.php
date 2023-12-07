e5449bbd416d1f12995fd8d9b9f89913e7b66064
Carlos Delgado <jd1005337@gmail.com>
2023-12-07 03:22:25 -0400

    corregido bugs de planilla
    se corrigio de planilla

--- resources/views/planillasueldo/indexEmpleado.blade.php
+++ resources/views/planillasueldo/indexEmpleado.blade.php
@@ -8,7 +8,7 @@
                     <div class="card-body">
                         <div class="d-flex justify-content-between align-items-center mb-3">
                             <h3 class="page__heading">LIQUIDAR</h3>
-                            <a class="btn btn-dark ml-auto" href="<!-- {{ route('planillasueldos.index') }} -->">Atras </a>
+                            <a class="btn btn-dark ml-auto" href="{{ route('planillasueldos.index') }}">Atras </a>
                         </div>
                         <div class="table-responsive">
                            
